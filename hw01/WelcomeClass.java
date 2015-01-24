53d3195dddfb9f491f36b24b98b754387737ee99
jwb218 <jwb218@lehigh.edu>
2015-01-24 23:01:10 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////
+//Josh Blatt
+//HW 01 Java Program
+
+
+// define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[ ] args) {
+        
+//      print the graphic and biographical statement
+        System.out.println("    -----------     ");
+        System.out.println("    | WELCOME |     ");
+        System.out.println("    -----------     ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-J--W--B--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("2 weeks ago, my family and I watched my favorite team, The Ohio State Buckeyes, win the national championship");
+    }
+}
